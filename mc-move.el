2771d9de2787cd959cf744b184bee14cd076d61f
Victor Makarov <vitja.makarov@gmail.com>
2020-11-14 22:06:10 +0300

    mc-move: add local and global minor modes

--- mc-move.el
+++ mc-move.el
@@ -1,12 +1,12 @@
-;;; mc-move.el --- Mcedit alike word movements for Emacs
+;;; mc-move.el --- Mcedit alike word movements for Emacs -*- lexical-binding: t; -*-
 
 ;; Keywords: tools
 
-;; Copyright (C) 2009-2017 Victor Makarov
+;; Copyright (C) 2009-2020 Victor Makarov
 
 ;; Author: Victor Makarov <vitja.makarov@gmail.com>
 ;; URL: https://github.com/vitek/configs/tree/master/emacs/site-lisp/mc-move.el
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,13 +23,30 @@
 
 ;;; Commentary:
 
-;; This package implements its own versions of `forward-word`, `backward-word`,
-;; `kill-word`, `backward-kill-word` which were originally inspired by mcedit
-;; behaviour.  It treats undersocre and capital letter as the begining of new
-;; word.  Which is very useful when working with variable and function names.
-;; To enable it just require:
+;; This package implements mc-move-mode which overrides `forward-word`,
+;; `backward-word`, `kill-word`, `backward-kill-word` functions with its
+;; own logic which were originally inspired by mcedit.
+;;
+;; It works this way:
+;; - when moving cursor is set at the begining of the word
+;; - word is an alpha-numeric sequence samecase or capitalized
+;; - symbols are threated as different kind of word
+;; - only whitespaces are skipped
+;;
+;; This all allow to easily navigate over the code.  You can stop at parts of
+;; under_score, CamelCase or minus-hyphen style words.  And delete that part
+;; backward and forward.
+;;
+;; In order enable it add the following commands to your Emacs config:
 ;;
 ;;  (require 'mc-move)
+;;  (global-mc-move-mode)
+;;
+;;  Or with use-package:
+;;
+;;  (use-package mc-move
+;;    :config
+;;    (global-mc-move-mode))
 
 ;;; Code:
 
@@ -40,59 +57,114 @@
 (defvar mc-move-upper-words "[:upper:][:digit:]")
 (defvar mc-move-lower-words "[:lower:][:digit:]")
 
+(defvar mc-move--installed nil
+  "Internal flag indicating that mc-move is installed.")
+
+(defconst mc-move--orig-forward-word (symbol-function 'forward-word))
+(defconst mc-move--orig-backward-word (symbol-function 'backward-word))
+(defconst mc-move--orig-kill-word (symbol-function 'kill-word))
+(defconst mc-move--orig-backward-kill-word
+  (symbol-function 'backward-kill-word))
+
+;;;###autoload
+(define-minor-mode mc-move-mode
+  "Toggle mc-move mode."
+  :init-value nil
+  :lighter ""
+  (mc-move-install))
+
+;;;###autoload
+(define-globalized-minor-mode global-mc-move-mode mc-move-mode mc-move-mode-on)
+
+(defun mc-move-mode-on ()
+  "Enable mc-move-mode."
+  (mc-move-mode 1))
+
 (defun mc-move-by-word (count)
   "Do the move COUNT times.  Negative COUNT for backward direction."
   (if (> count 0)
-      (dotimes (i count)
+      (dotimes (_ count)
         (progn
           (cond
            ((> (skip-chars-forward mc-move-delims) 0) 1)
            ((> (+ (skip-chars-forward mc-move-upper-words)
-                  (skip-chars-forward mc-move-lower-words)) 0)
+                  (skip-chars-forward mc-move-lower-words))
+               0)
             (skip-chars-forward mc-move-spaces))
            ((skip-chars-forward mc-move-specials)
             (skip-chars-forward mc-move-delims))))))
-  (dotimes (i (- count))
+  (dotimes (_ (- count))
     (progn
       (skip-chars-backward mc-move-spaces)
       (cond
        ((< (skip-chars-backward "\n") 0) 1)
        ((< (+ (skip-chars-backward mc-move-lower-words)
-              (skip-chars-backward mc-move-upper-words)) 0) 2)
+              (skip-chars-backward mc-move-upper-words))
+           0)
+        2)
        ((skip-chars-backward mc-move-specials) 0)))))
 
 (defun mc-move-forward-word (&optional arg)
   "Custom version of `forward-word`.
 With argument ARG, do this that many times."
   (interactive "p")
-  (if arg
-      (mc-move-by-word arg)
-    (mc-move-by-word 1)))
+  (if mc-move-mode
+      (if arg
+          (mc-move-by-word arg)
+        (mc-move-by-word 1))
+    (funcall mc-move--orig-forward-word arg)))
 
 (defun mc-move-backward-word (&optional arg)
   "Custom version of `backword-word`.
 With argument ARG, do this that many times."
   (interactive "p")
-  (if arg
-      (mc-move-by-word (- arg))
-    (mc-move-by-word -1)))
+  (if mc-move-mode
+      (if arg
+          (mc-move-by-word (- arg))
+        (mc-move-by-word -1))
+    (funcall mc-move--orig-backward-word arg)))
 
 (defun mc-move-kill-word (&optional arg)
   "Custom version of `kill-word`.
 With argument ARG, do this that many times."
   (interactive "p")
-  (kill-region (point) (progn (mc-move-forward-word arg) (point))))
+  (if mc-move-mode
+      (kill-region (point) (progn (mc-move-forward-word arg) (point)))
+    (funcall mc-move--orig-kill-word arg)))
 
 (defun mc-move-backward-kill-word (&optional arg)
   "Custom version of `backward-kill-word`.
 With argument ARG, do this that many times."
   (interactive "p")
-  (mc-move-kill-word (- arg)))
+  (if mc-move-mode
+      (mc-move-kill-word (- arg))
+    (funcall mc-move--orig-backward-kill-word arg)))
 
-(defalias 'forward-word 'mc-move-forward-word)
-(defalias 'backward-word 'mc-move-backward-word)
-(defalias 'kill-word 'mc-move-kill-word)
-(defalias 'backward-kill-word 'mc-move-backward-kill-word)
+(defun mc-move-install ()
+  "Override Emacs 'move by word' functions with `mc-move` versions.
+
+Overrides: 'forward-word, 'backward-word, 'kill-word, 'backward-kill-word"
+  (interactive)
+  (when (not mc-move--installed)
+    (fset 'forward-word 'mc-move-forward-word)
+    (fset 'backward-word 'mc-move-backward-word)
+    (fset 'kill-word 'mc-move-kill-word)
+    (fset 'backward-kill-word 'mc-move-backward-kill-word)
+    (setq mc-move--installed t)))
+
+(defun mc-move-uninstall ()
+  "Go back to Emacs original functions overridden by 'mc-move-install."
+  (interactive)
+  (when mc-move--installed
+    (fset 'forward-word mc-move--orig-forward-word)
+    (fset 'backward-word mc-move--orig-backward-word)
+    (fset 'kill-word mc-move--orig-kill-word)
+    (fset 'backward-kill-word mc-move--orig-backward-kill-word)
+    (setq mc-move--installed nil)))
+
+(defun mc-move-unload-function ()
+  "Unload the mc-move library."
+  (mc-move-uninstall))
 
 (provide 'mc-move)
 ;;; mc-move.el ends here
